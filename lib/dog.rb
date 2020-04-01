7616ecba51fa0b8e61e1adb1f0a4bee8981386a0
Joshua Williams <jmw0426@hotmail.com>
2020-04-01 21:36:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
 class Dog 
   def initialize(name, breed)
-    @name = name
-    @breed = breed
     if breed == 0 
       return "Mutt"
+    
+    @name = name
+    @breed = breed
+    
   end
 end
   
